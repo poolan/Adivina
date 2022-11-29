8e65835e42568abd5a75e3abfe05b5551ceceee0
raul fernandez <raulpoolan@gmail.com>
2022-11-29 22:01:36 +0200

    asd

--- App.js
+++ App.js
@@ -1,46 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createNativeStackNavigator();
-
-function HomeScreen({ navigation }) {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Home!</Text>
-      <Button title="Play" onPress={() => navigation.navigate("Play")} />
+    <View>
+      <Text>This is Adivina</Text>
     </View>
   );
-}
+};
 
-function PlayScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>
-        Lets try to retrieve here a food information API and allocate its price
-        into asneed to guess.1
-      </Text>
-    </View>
-  );
-}
+export default App;
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Play" component={PlayScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
