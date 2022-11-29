5d94215d10e47475c05fcda7f69f8a826d2343ae
raul fernandez <raulpoolan@gmail.com>
2022-11-29 22:59:13 +0200

    App

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen.js";
+import Questions from "./src/screens/Questions.js";
+import Result from "./src/screens/Result.js";
 
 const App = () => {
   return (
     <View>
-      <Text>This is Adivina</Text>
+      <Text style={styles.container}>
+        <HomeScreen />
+        {/* <Questions /> */}
+        {/* <Result /> */}
+      </Text>
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 12,
+    paddingTop: 30,
+  },
+});
